5ce41b101ff5d99258c653c8754bacff684b3122
Paul Gierz <pgierz@awi.de>
2021-10-07 12:31:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-pipreqs --force --savepath $INPUT_REQUIREMENT_PATH $INPUT_PROJECT_PATH
+pipreqs --force --no-pin --savepath $INPUT_REQUIREMENT_PATH $INPUT_PROJECT_PATH
 
 set -e
 sh -c "ls"
